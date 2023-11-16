f97c52cd9898a1ffdcee547ed270efb87a962000
Ambre Bidault <ambrebidault@orange.fr>
2023-11-16 21:14:46 +0100

    presentation mircodonnées

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -3,15 +3,13 @@ import memin from '../../images/memin.jpg'
 
 const Presentation = (PresentationDatas) => {
     return (
-        <section className="section" id="presentation" itemType = "https://schema.org/PostalAddress">
+        <section className="section" id="presentation" itemScope itemType="http://schema.org/LocalBusiness">
             <div className="presentation">
                 <h2 className="section__title">{ PresentationDatas.title }</h2>
                 <motion.h3 initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.8}}  className="section__title--subtitle" itemProp = "name" > Ambre Bidault </motion.h3>
                 <motion.img itemProp="image" className="section__title--photoPresentation" src={ memin } alt="Ambre Bidault" initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.8}}/>
             </div>
-            <motion.div 
-                initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.8}} className="description__presentation"
-                itemScope itemType="http://schema.org/LocalBusiness">
+            <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 0.8}} className="description__presentation" itemType = "https://schema.org/PostalAddress">
                 <div className="description__reflocal" itemProp = "address">   
                     <p>{ PresentationDatas.description }</p><br></br>
                     <span itemProp = "streetAddress"> Adresse : {PresentationDatas.address}, </span>
